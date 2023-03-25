a4a78d160d3b35e4e4743781f4786abdaf84a559
piyush-agrawal6 <agrawaljoy1@gmail.com>
2023-03-25 09:03:33 +0530

    redux integrated

--- src/features/Tasks/taskRoute.js
+++ src/features/Tasks/taskRoute.js
@@ -24,27 +24,6 @@ app.post("/", async (req, res) => {
   }
 });
 
-// app.put("/", async (req, res) => {
-//   try {
-//     const { id, userId, quantity } = req.body;
-//     const cartItem = await Cart.findById(id);
-//     if (cartItem && cartItem.userId.toString() === userId) {
-//       const cart = await Cart.findByIdAndUpdate(
-//         id,
-//         { userId, productId: cartItem.productId, quantity },
-//         { new: true }
-//       )
-//         .populate("productId")
-//         .select("-userId");
-//       return res.status(200).send({ message: "Cart updated successfully" });
-//     } else {
-//       return res.status(404).send({ message: "Item does not exist in cart" });
-//     }
-//   } catch (error) {
-//     return res.status(404).send({ message: "Something went wrong" });
-//   }
-// });
-
 app.delete("/delete", async (req, res) => {
   try {
     const id = req.query.id;
@@ -57,6 +36,21 @@ app.delete("/delete", async (req, res) => {
     }
   } catch (error) {
     return res.status(404).send({ message: error });
+  }
+});
+
+//update user
+app.put("/update", async (req, res) => {
+  let { id } = req.body;
+  let user = await User.findOne({ _id: id });
+  try {
+    const task = await Task.findByIdAndUpdate(id, {
+      ...req.body,
+      assignee: user,
+    });
+    return res.status(200).send({ message: "Task updated successfully", task });
+  } catch (error) {
+    return res.status(404).send({ error: error.message });
   }
 });
 
